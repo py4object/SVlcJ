d740b627a5914e9a0163233ae3888ba0a1da99c6
Omar Zahed <py4object@gmail.com>
2015-06-30 13:21:20 +0300

    serializable events to be sent/recived on the clinet/server

---
+++ src/main/java/uk/co/caprica/vlcjplayer/view/synchornization/SynchronizeFrame.java
@@ -0,0 +1,37 @@
+package uk.co.caprica.vlcjplayer.view.synchornization;
+
+import com.google.common.eventbus.Subscribe;
+import uk.co.caprica.vlcjplayer.ShowSynchronizeFrameEvent;
+import uk.co.caprica.vlcjplayer.view.BaseFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by kozo on 6/30/15.
+ */
+public class SynchronizeFrame extends BaseFrame {
+    private JButton jbConnect;
+    private JTextField jtServer;
+    private JTextField jtPort;
+    public SynchronizeFrame(){
+        super("Synchronize");
+        JPanel panel=new JPanel();
+        jbConnect =new JButton("Connect");
+        jtServer=new JTextField(10);
+        jtPort =new JTextField(5);
+        panel.setLayout(new GridLayout(3,2));
+        panel.add(new JLabel("Sever:"));
+        panel.add(new JLabel("Port:"));
+        panel.add(jtServer);
+        panel.add(jtPort);
+        panel.add(jbConnect);
+        add(panel);
+        pack();
+    }
+    @Subscribe
+    public void ShowFrame(ShowSynchronizeFrameEvent e){
+        setVisible(true);
+    }
+
+}
