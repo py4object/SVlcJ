adf5ceb9f18ca61a5cb63ac30d63b3a67ede316b
Omar Zahed <py4object@gmail.com>
2015-06-30 13:19:15 +0300

    to use as a signal

---
+++ src/main/java/uk/co/caprica/vlcjplayer/ShowSynchronizeFrameEvent.java
@@ -0,0 +1,8 @@
+package uk.co.caprica.vlcjplayer;
+
+/**
+ * Created by kozo on 6/30/15.
+ */
+public class ShowSynchronizeFrameEvent {
+    public static final ShowSynchronizeFrameEvent INSTANCE=new ShowSynchronizeFrameEvent();
+}
